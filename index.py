e9b887bf51937cb9ae47e484ea83771bb857806c
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-10-05 14:59:17 +0800

    update 1005-14:59

--- index.py
+++ index.py
@@ -11,4 +11,10 @@ def index():
 def feature():
     return render_template("feature.jinja.html")
 
+@app.route("/description")
+def description():
+    return render_template("description.jinja.html")
 
+@app.route("/product")
+def product():
+    return render_template("product.jinja.html")
