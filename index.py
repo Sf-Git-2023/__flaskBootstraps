d0c4920a62e5c57109b3a77c6b49d35491daa9b4
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-10-05 13:51:20 +0800

    update 1005-13:51

--- index.py
+++ index.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.jinja.html")
+
+@app.route("/feature")
+def feature():
+    return render_template("feature.jinja.html")
+
